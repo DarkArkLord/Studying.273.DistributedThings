b82d44e269104cf222fdc8043f1236156d5de957
DarkLord <dfhrhfan12@yandex.ru>
2024-03-22 20:53:06 +0400

    Add partition test

---
+++ DistributedThings/PartitionTest/ApplicationContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PartitionTest
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<TRecordMain> RecordsMain { get; set; }
+        public DbSet<TRecordRepl_1> RecordsRepl1 { get; set; }
+        public DbSet<TRecordRepl_2> RecordsRepl2 { get; set; }
+        public DbSet<TRecordRepl_3> RecordsRepl3 { get; set; }
+
+        public ApplicationContext()
+        {
+            //
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost; Port=6601; Database=db; Username=postgres; Password=pospas");
+        }
+    }
+
+    [Table("partition_test_main")]
+    public class TRecordMain
+    {
+        public int id { get; set; }
+        public int value { get; set; }
+    }
+
+    [Table("partition_test_1")]
+    public class TRecordRepl_1 : TRecordMain { }
+
+    [Table("partition_test_2")]
+    public class TRecordRepl_2 : TRecordMain { }
+
+    [Table("partition_test_3")]
+    public class TRecordRepl_3 : TRecordMain { }
+}
---
+++ DistributedThings/PartitionTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartitionTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using (var db = new ApplicationContext())
+            {
+                Console.WriteLine("Db content after creating");
+                WriteRecords(db.RecordsMain.ToArray());
+
+                var record1 = new TRecordMain { id = 1, value = 111 };
+                db.RecordsMain.Add(record1);
+                var record2 = new TRecordMain { id = 1, value = 444 };
+                db.RecordsMain.Add(record2);
+                var record3 = new TRecordMain { id = 1, value = 777 };
+                db.RecordsMain.Add(record3);
+
+                db.SaveChanges();
+
+                Console.WriteLine("Db content after add records");
+                WriteRecords(db.RecordsMain.ToArray());
+            }
+
+            Console.WriteLine("\n----------\nEnd!");
+        }
+
+        static void WriteRecords(IEnumerable<TRecordMain> records)
+        {
+            foreach (var record in records)
+            {
+                Console.WriteLine($"{record.id}) {record.value}");
+            }
+        }
+    }
+}
