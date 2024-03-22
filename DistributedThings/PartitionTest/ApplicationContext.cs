183f8410e565cef0a56246556f1eb3d974b16a32
DarkLord <dfhrhfan12@yandex.ru>
2024-03-22 21:01:22 +0400

    Upd partition test

--- DistributedThings/PartitionTest/ApplicationContext.cs
+++ DistributedThings/PartitionTest/ApplicationContext.cs
@@ -17,23 +17,42 @@ namespace PartitionTest
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost; Port=6601; Database=db; Username=postgres; Password=pospas");
+            optionsBuilder.UseNpgsql("Host=localhost; Port=6601; Database=postgres; Username=postgres; Password=pospas");
         }
     }
 
+    public interface IRecord
+    {
+
+        public int id { get; }
+        public int value { get; }
+    }
+
     [Table("partition_test_main")]
-    public class TRecordMain
+    public class TRecordMain : IRecord
     {
         public int id { get; set; }
         public int value { get; set; }
     }
 
     [Table("partition_test_1")]
-    public class TRecordRepl_1 : TRecordMain { }
+    public class TRecordRepl_1 : IRecord
+    {
+        public int id { get; set; }
+        public int value { get; set; }
+    }
 
     [Table("partition_test_2")]
-    public class TRecordRepl_2 : TRecordMain { }
+    public class TRecordRepl_2 : IRecord
+    {
+        public int id { get; set; }
+        public int value { get; set; }
+    }
 
     [Table("partition_test_3")]
-    public class TRecordRepl_3 : TRecordMain { }
+    public class TRecordRepl_3 : IRecord
+    {
+        public int id { get; set; }
+        public int value { get; set; }
+    }
 }
--- DistributedThings/PartitionTest/Program.cs
+++ DistributedThings/PartitionTest/Program.cs
@@ -13,26 +13,42 @@ namespace PartitionTest
 
             using (var db = new ApplicationContext())
             {
-                Console.WriteLine("Db content after creating");
+                foreach(var record in db.RecordsMain)
+                {
+                    db.RecordsMain.Remove(record);
+                }
+
+                db.SaveChanges();
+
+                Console.WriteLine("Main content after cleaning");
                 WriteRecords(db.RecordsMain.ToArray());
 
                 var record1 = new TRecordMain { id = 1, value = 111 };
                 db.RecordsMain.Add(record1);
-                var record2 = new TRecordMain { id = 1, value = 444 };
+                var record2 = new TRecordMain { id = 2, value = 444 };
                 db.RecordsMain.Add(record2);
-                var record3 = new TRecordMain { id = 1, value = 777 };
+                var record3 = new TRecordMain { id = 3, value = 777 };
                 db.RecordsMain.Add(record3);
 
                 db.SaveChanges();
 
-                Console.WriteLine("Db content after add records");
+                Console.WriteLine("Main content after add records");
                 WriteRecords(db.RecordsMain.ToArray());
+
+                Console.WriteLine("Repl 1 content after add records");
+                WriteRecords(db.RecordsRepl1.ToArray());
+
+                Console.WriteLine("Repl 2 content after add records");
+                WriteRecords(db.RecordsRepl2.ToArray());
+
+                Console.WriteLine("Repl 3 content after add records");
+                WriteRecords(db.RecordsRepl3.ToArray());
             }
 
             Console.WriteLine("\n----------\nEnd!");
         }
 
-        static void WriteRecords(IEnumerable<TRecordMain> records)
+        static void WriteRecords(IEnumerable<IRecord> records)
         {
             foreach (var record in records)
             {
