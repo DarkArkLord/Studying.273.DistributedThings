7325a3e8812d97ea5bfc22eda6c09e3b478e4195
DarkLord <dfhrhfan12@yandex.ru>
2024-03-22 20:36:50 +0400

    Add replication test

---
+++ DistributedThings/ReplicationTest/ApplicationContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReplicationTest
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<TRecord> Records { get; set; }
+
+        private DbInstance instance;
+
+        public ApplicationContext(DbInstance instance, bool delete, bool create)
+        {
+            bool? wasDeleted = null;
+            bool? wasCreated = null;
+
+            if (delete)
+            {
+                wasDeleted = Database.EnsureDeleted();
+            }
+
+            if (create)
+            {
+                wasCreated = Database.EnsureCreated();
+            }
+
+            this.instance = instance;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(instance.GetConnectionString());
+        }
+    }
+
+    public class TRecord
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public enum DbInstance
+    {
+        Master = 0,
+        Slave = 1,
+    }
+
+    public static class DbInstanceExt
+    {
+        public static string GetConnectionString(this DbInstance instance)
+        {
+            switch (instance)
+            {
+                case DbInstance.Master:
+                    return "Host=localhost; Port=6601; Database=db; Username=postgres; Password=pospas";
+                case DbInstance.Slave:
+                    return "Host=localhost; Port=6602; Database=db; Username=postgres; Password=pospas";
+            }
+            return null;
+        }
+    }
+}
---
+++ DistributedThings/ReplicationTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReplicationTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using (var db = new ApplicationContext(DbInstance.Master, true, true))
+            {
+                Console.WriteLine("Db content on master after creating");
+                WriteRecords(db.Records.ToArray());
+
+                var record1 = new TRecord { Id = 1, Text = "123" };
+                db.Records.Add(record1);
+                var record2 = new TRecord { Id = 2, Text = "456" };
+                db.Records.Add(record2);
+                var record3 = new TRecord { Id = 3, Text = "789" };
+                db.Records.Add(record3);
+
+                db.SaveChanges();
+
+                Console.WriteLine("Db content on master after add records");
+                WriteRecords(db.Records.ToArray());
+            }
+
+            Task.Delay(1000).Wait();
+
+            using (var db = new ApplicationContext(DbInstance.Slave, false, false))
+            {
+                Console.WriteLine("Db content on slave");
+                WriteRecords(db.Records.ToArray());
+            }
+
+            Console.WriteLine("\n----------\nEnd!");
+        }
+
+        static void WriteRecords(IEnumerable<TRecord> records)
+        {
+            foreach (var record in records)
+            {
+                Console.WriteLine($"{record.Id}) {record.Text}");
+            }
+        }
+    }
+}
